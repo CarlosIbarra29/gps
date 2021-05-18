8b112c0a66d903796c12d3f737b6a81be57c1f29
Fernando <Fernando>
2021-05-18 16:53:46 -0500

    vista asignar cuadrilla

--- application/controllers/PersonalController.php
+++ application/controllers/PersonalController.php
@@ -11,18 +11,28 @@ class PersonalController extends Zend_Controller_Action{
         $this->_user = new Application_Model_GpsUserModel;
         $this->_panel = new Application_Model_GpsPanelModel;
         $this->_sitio = new Application_Model_GpsSitioModel;
-        $this->_project = new Application_Model_GpsProyectoModel;
-        $this->_servicios = new Application_Model_GpsServicioModel;
-        $this->_ordencompra = new Application_Model_GpsSolicitudOrdenModel;
-        $this->_comprobacion = new Application_Model_GpsComprobacionModel;
-        $this->_gasolina = new Application_Model_GpsGasolinaModel;
-        if(empty($this->_session->id)){
-            $this->redirect('/home/login');
-        }    
+        $this->_personal = new Application_Model_GpsPersonalModel;
+
+        if(empty($this->_session->id)){ $this->redirect('/home/login'); }    
     }
 
     public function listapersonalAction(){
-        
+        $actualpagina=$this->_getParam('pagina');
+        $this->view->actpage=$actualpagina;
+
+        $id=1; $wh="status_campo";
+        $table="personal_campo";
+        $pagi_count = $this->_season->GetSpecific($table,$wh,$id);
+        $count=count($pagi_count);
+        if (isset($_GET['pagina'])) { $pagina = $_GET['pagina']; } else { $pagina= $this->view->pagina = 1;} 
+
+        $no_of_records_per_page = 20;
+        $offset = ($pagina-1) * $no_of_records_per_page; 
+        $total_pages= $count;
+
+        $this->view->totalpage = $total_pages;
+        $this->view->total=ceil($total_pages/$no_of_records_per_page);
+        $this->view->paginator= $this->_personal->getpersonalcuadrilla($offset,$no_of_records_per_page,$id); 
     }
 
 }
---
+++ application/models/GpsPersonalModel.php
@@ -0,0 +1,26 @@
+<?php
+
+class Application_Model_GpsPersonalModel extends Zend_Db_Table_Abstract{
+
+    public function getpersonalcuadrilla($offset,$no_of_records_per_page,$id){
+        try{
+            $db = Zend_Db_Table::getDefaultAdapter();
+            $qry = $db->query("SELECT pc.id, pc.nombre, pc. apellido_pa, pc.apellido_ma, pc.imagen, pc.curp, pc.puesto,
+                                pc.status_expediente, pc.telefono, pc.email_personal, pc.nss, pc.rfc, pc.dia_pago, 
+                                pc.hora_pago, pc.status_personal, pc.fecha_personal, pc.status_cuadrilla, 
+                                pc.tipo_proyectopersonal, pc.sitio_tipoproyectopersonal, pc. id_sitiopersonal, 
+                                pc.name_sitio,pc.delete_status ,pp.id as idpuesto, pp.nombre as name_puesto,  
+                                pc.status_operativo , pc.status_campo
+                                FROM personal_campo pc 
+                                LEFT JOIN puestos_personal pp on pc.puesto = pp.id 
+                                WHERE pc.status_campo = ? 
+                                LIMIT $offset,$no_of_records_per_page",array($id));
+            $row = $qry->fetchAll();
+            return $row;
+            $db->closeConnection();
+        }catch (Exception $e){
+            echo $e;
+        }
+    } //END GET PAGINATOR PERSONAL
+
+}
